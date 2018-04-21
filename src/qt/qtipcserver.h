09be45ffa7e4cfe1545588d8036aab89c2bfc086
LakrasscoinLKR <nenex2424@gmail.com>
2018-04-21 19:07:29 -0400

    LKR

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define LakrassCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "LakrassCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
